75ad3b1be43cb44251b9b1f71096b1faec720eef
Daniel Obando <obandovasquezdanielisaac@gmail.com>
2026-03-01 13:20:01 -0600

    añadiendo filtros, y nuevos menus

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import classes.Status;
 import lists.GameStorage;
 import util.ScannerUtil;
 
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args){
@@ -32,11 +35,11 @@ public class Main {
                         Status status = Status.valueOf(ScannerUtil.captureText("ingrese el estatus"));
                         Platform platform = Platform.valueOf(ScannerUtil.captureText("ingrese el platform"));
                         String developer = ScannerUtil.captureText("ingrese el developer");
-                        int releseYear = ScannerUtil.captureNumber("ingrese año de lanzamiento");
+                        int releaseYear = ScannerUtil.captureNumber("ingrese año de lanzamiento");
                         double rating = ScannerUtil.captureDecimal("califique el juego");
                         double hoursPlayed = ScannerUtil.captureDecimal("ingrese la cantidad de horas jugadas");
                         // guardando los datos en el objeto
-                        Game gm = new Game(title, genre ,platform,developer,status, releseYear, rating, hoursPlayed);
+                        Game gm = new Game(title, Set.of(genre),Set.of(platform),developer,status, releaseYear, rating, hoursPlayed);
 
                         //si son validos se guardan en la lista
                         if(gm.isValid()){
@@ -50,10 +53,10 @@ public class Main {
                     break;
                 case 2:
                     option=1;
-                    while(option!=2){
+                    while(option!=2 && option!=0){
                         System.out.println("opcion 2 mostrar lista");
-                        gmst.showGames();
-                        option = ScannerUtil.menuList();
+                        System.out.println(gmst.toString());
+                            option = ScannerUtil.menuList();
                     }
 
                     break;
@@ -78,7 +81,79 @@ public class Main {
                     }
                     break;
                 case 6:
-                    System.out.println("opcion 6");
+                    System.out.println("opcion 6, serch, filter sort");
+                    int ans;
+                    do{
+                         ans= ScannerUtil.ser_fil_sor_Menu();
+                        switch(ans){
+                            case 1:
+                                do{
+                                    String text=ScannerUtil.captureText("ingrese el titulo del juego o el developer");
+                                    gmst.searchOptions(text);
+                                    ans = ScannerUtil.searchMenu();
+                                }while (ans==1);
+
+                                break;
+                                case 2:
+                                    do {
+                                        ans = ScannerUtil.filterMenu();
+                                        if (ans==1){
+                                            try {
+                                                String genre =  ScannerUtil.captureText("ingrese el genero");
+                                                gmst.filterOptionGenre(Genre.valueOf(genre));
+                                            }catch (IllegalArgumentException e){
+                                                System.out.println("Genero no valido");
+
+                                            }
+
+                                        }else if(ans==2){
+                                            try {
+                                                String platform =  ScannerUtil.captureText("ingrese la plataforma");
+                                                gmst.filterOptionPlatfom(Genre.valueOf(platform));
+                                            }catch (IllegalArgumentException e){
+                                                System.out.println("Plataforma no valida");
+
+                                            }
+
+                                        }else if(ans==3){
+                                            try {
+                                                String status =  ScannerUtil.captureText("ingrese el status");
+                                                gmst.filterOptionStatus(Genre.valueOf(status));
+                                            }catch (IllegalArgumentException e){
+                                                System.out.println("Status no valido");
+                                            }
+
+                                        }else{
+
+                                            System.out.println("Opcion no valida");
+                                            ans=0;
+                                        }
+
+                                    }while (ans<4);
+                                    break;
+                                    case 3:
+                                        do{
+                                            ans=ScannerUtil.sortMenu();
+                                            if (ans==1){
+                                                gmst.sortOptionReleaseYear();
+                                            }else if(ans==2){
+                                                gmst.sortOptionTitle();
+                                            }else if(ans==3){
+                                                gmst.sortOptionRating();
+                                            }else{
+                                                System.out.println("Opcion no valida");
+                                                ans=0;
+                                            }
+                                        }while(ans<4);
+
+                                        break;
+                                        default:
+                                            System.out.println("Opcion no valida");
+                                            ans=0;
+                                            break;
+                        }
+
+                    }while(ans<4);
                     break;
                 case 7:
                     System.out.println("opcion 7");
@@ -101,11 +176,61 @@ public class Main {
 
     }
     public static void cargarGame(GameStorage game){
-        game.addGame(new Game("The Witcher 3", Genre.RPG, Status.COMPLETED, 2015, 120.5, 4.9));
-        game.addGame(new Game("FIFA 24", Genre.RPG, Status.COMPLETED,2023, 35.0, 4.2));
-        game.addGame(new Game("Minecraft", Genre.RPG, Status.COMPLETED, 2011, 250.0, 5.0));
-        game.addGame(new Game("Cyberpunk 2077", Genre.RPG, Status.COMPLETED, 2020, 60.0, 4.0));
-        game.addGame(new Game("Resident Evil 4", Genre.RPG, Status.COMPLETED, 2005, 40.0, 4.8));
+
+        game.addGame(new Game(
+                "The Witcher 3",
+                Set.of(Genre.RPG, Genre.ADVENTURE),
+                Set.of(Platform.PC),
+                "CD Projekt Red",
+                Status.COMPLETED,
+                2015,
+                120.5,
+                4.9
+        ));
+
+        game.addGame(new Game(
+                "FIFA 24",
+                Set.of(Genre.SPORTS),
+                Set.of(Platform.PLAYSTATION),
+                "EA Sports",
+                Status.PLAYING,
+                2023,
+                35.0,
+                4.2
+        ));
+
+        game.addGame(new Game(
+                "Minecraft",
+                Set.of(Genre.SIMULATION),
+                Set.of(Platform.PC),
+                "Mojang",
+                Status.IN_LIBRARY,
+                2011,
+                250.0,
+                5.0
+        ));
+
+        game.addGame(new Game(
+                "Cyberpunk 2077",
+                Set.of(Genre.RPG),
+                Set.of(Platform.PC),
+                "CD Projekt Red",
+                Status.DOWNLOADED,
+                2020,
+                60.0,
+                4.0
+        ));
+
+        game.addGame(new Game(
+                "Resident Evil 4",
+                Set.of(Genre.HORROR),
+                Set.of(Platform.PLAYSTATION),
+                "Capcom",
+                Status.COMPLETED,
+                2005,
+                40.0,
+                4.8
+        ));
 
     }
 }
